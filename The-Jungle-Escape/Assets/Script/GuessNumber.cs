61dee81b65aa5c1a947edfa56082caf909e67272
Ankit Kumar <ankitkumar36905@gmail.com>
2019-08-09 23:37:36 +0530

    Added Project Files
    Project Updated

---
+++ The-Jungle-Escape/Assets/Script/GuessNumber.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GuessNumber : MonoBehaviour
+{
+
+    [SerializeField] int min;
+    [SerializeField] int max;
+    [SerializeField] TextMeshProUGUI guessText;
+
+    int guess;
+    int flag = 0;
+
+    public void StartGame()
+    {
+        if(flag==0)
+        {
+            ++max;
+            UpdateGuess();
+        }
+    }
+
+    public void FindLowerNumber()
+    {
+        if(flag==1)
+        {
+            max = guess;
+            UpdateGuess();
+        } 
+    }
+
+    public void FindHigherNumber()
+    {
+        if(flag==1)
+        {
+            min = guess + 1;
+            UpdateGuess();
+        }
+    }
+
+    private void UpdateGuess()
+    {
+        guess = Random.Range(min,max);
+        guessText.text = guess.ToString();
+        flag = 1;
+    }
+}
---
+++ The-Jungle-Escape/Assets/Script/SceneLoader.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+   public void LoadNextScene()
+   {
+        // Get count of number of scenes in Build Settings
+        int scenes = SceneManager.sceneCountInBuildSettings;
+
+        // Get index of currently active scene
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+
+        // condition to load next state. False when we are on the last scene
+        if(currentSceneIndex!=scenes-1)
+        {
+            SceneManager.LoadScene(currentSceneIndex + 1);
+        }
+        else
+        {
+            SceneManager.LoadScene(0);
+        }
+   }
+}
